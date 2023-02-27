f4f807c8f5c76623d5a9b41b2c58e162f9ff06b3
davidecardelli <davide.cardelli.94@gmail.com>
2023-02-27 20:47:20 +0100

    end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Model Control</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -16,7 +16,34 @@
 </head>
 
 <body>
-<h1>prova</h1>
+
+    <header class="bg-black text-center py-3 mb-5">
+        <h1 class="text-white">Movies</h1>
+    </header>
+
+
+    <main>
+        <div class="container">
+            <div class="row row-cols-4">
+                @foreach ($movies as $movie)
+                    <div class="col">
+                        <div class="card text-center mb-5">
+                            <img class="img-fluid" src="{{ $movie['poster'] }}" alt="{{ $movie['title'] }}"
+                                style="height: 25rem">
+                            <div class="card-body">
+                                <h6 class="card-title mb-3">{{ $movie['title'] }}</h6>
+                                <h6 class="card-title mb-3">{{ $movie['original_title'] }}</h6>
+                                <p class="card-text mb-3">{{ $movie['nationality'] }}</p>
+                                <p class="card-text mb-3">{{ $movie['date'] }}</p>
+                                <p class="card-text mb-3">{{ $movie['vote'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
